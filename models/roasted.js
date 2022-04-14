921dc8d0e2af156a10f745f4869220e0c276f35b
s546553 <98134539+s546553@users.noreply.github.com>
2022-04-13 21:12:49 -0500

    added code

--- models/roasted.js
+++ models/roasted.js
@@ -1,25 +1,8 @@
 const mongoose = require("mongoose")
-const RoastedSchema = mongoose.Schema({
+const roastedSchema = mongoose.Schema({
 Itemname: String,
 Quantity: Number,
 price: String
 })
-module.exports = mongoose.model("Roasted",
-RoastedSchema)
-
-// We can seed the collection if needed on
-//server start
-async function recreateDB(){
- // Delete everything
-
- await Roasted.deleteMany();
- let instance1 = new
-Roasted({Itemname:"roasted sandwich", Quantity:90,
-price:"Fifteen USD"});
- instance1.save( function(err,doc) {
- if(err) return console.error(err);
- console.log("First object saved")
- });
-}
-let reseed = true;
-if (reseed) { recreateDB();}
+module.exports = mongoose.model("roasted",
+roastedSchema)
